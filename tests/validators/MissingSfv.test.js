36df4999c6b15ee9732b33892c9a874e3606c97d
doob <doobnet@gmail.com>
2020-04-18 21:13:16 +0200

    Fix #8: Issue with false SFV positive matching MP3 regex

--- tests/validators/MissingSfv.test.js
+++ tests/validators/MissingSfv.test.js
@@ -23,4 +23,13 @@ describe('Extra SFV validator', () => {
 
 		expect(scanner.errors.count('sfv_missing')).toEqual(0);
 	});
+
+	test('should not detect false SFV positives when non MP3 file matches MP3 regex', async () => {
+		const scanPath = path.join(__dirname, 'MissingSfv-data/False.Positive.Mp3-TEST');
+
+		const scanner = Scanner([ MissingSfv ], errorLogger);
+		await scanner.scanPath(scanPath);
+
+		expect(scanner.errors.count('sfv_missing')).toEqual(0);
+	});
 });
